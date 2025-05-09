030c74734168c0fb08120e50f23d4a02e5048724
kanakip <pallavikanaki13@gmail.com>
2025-05-09 13:03:37 +0530

    Create OOPS concept.java

---
+++ Java/OOPS concept.java
@@ -0,0 +1,13 @@
+What is the OOPs Concept?
+Object-oriented programming is one of the fundamental concepts of programming helping developers to bind functions and data together with the help of objects and classes.
+The OOPs concept in Java assists the programmers in controlling and accessing the data and implementing this to improve the readability and reusability of the code. 
+In Java, every class has some objects. Every object has some methods and properties associated with it. Combining them will build the structure of object-oriented programming.
+
+Major concepts -
+• Abstraction
+• Encapsulation
+• Inheritance
+• Polymorphism
+
+
+
