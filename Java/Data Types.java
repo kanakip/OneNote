d3280a05e2c36c95e99f7d1296c9cb1e127a3f31
kanakip <pallavikanaki13@gmail.com>
2025-05-09 13:22:17 +0530

    Create Data Types.java

---
+++ Java/Data Types.java
@@ -0,0 +1,16 @@
+2. Non-Primitive Data Type or Object Data type
+Primitive Data Type: Primitive data are single values with no special capabilities. There are 8 primitive data types:
+	• boolean: stores value true or false
+	• byte: stores an 8-bit signed two’s complement integer
+	• char: stores a single 16-bit Unicode character
+	• short: stores a 16-bit signed two’s complement integer
+	• int: stores a 32-bit signed two’s complement integer
+	• long: stores a 64-bit two’s complement integer
+	• float: stores a single-precision 32-bit IEEE 754 floating-point
+	• double: stores a double-precision 64-bit IEEE 754 floating-point
+Non-Primitive Data Type: Reference Data types will contain a memory address of the variable’s values because it is not able to directly store the values in the memory. Types of Non-Primitive are mentioned below:
+	• Strings
+	• Array
+	• Class
+	• Object
+	• Interface
