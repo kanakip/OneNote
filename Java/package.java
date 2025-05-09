da9a1fea8e5b07b7265559634b02f8446d79a9c9
kanakip <pallavikanaki13@gmail.com>
2025-05-09 13:21:40 +0530

    Create package.java

---
+++ Java/package.java
@@ -0,0 +1,33 @@
+14. What are the advantages of Packages in Java?
+There are various advantages of defining packages in Java.
+• Packages avoid name clashes.
+• The Package provides easier access control.
+• We can also have the hidden classes that are not visible outside and are used by the package.
+• It is easier to locate the related classes.
+15. How many types of packages are there in Java?
+There are two types of packages in Java
+• User-defined packages
+• Build In packages
+Common Built-in Packages:
+ 
+1. java.lang
+	• Automatically imported
+	• Contains fundamental classes
+	• Examples: String, System, Object
+2. java.util
+	• Collection framework
+	• Date/Time facilities
+	• Examples: ArrayList, HashMap, Scanner
+3. java.io
+	• Input/Output operations
+	• File handling
+	• Examples: File, InputStream, BufferedReader
+4. java.net
+	• Networking functionalities
+	• Examples: URL, Socket
+5. java.sql
+	• Database connectivity
+	• JDBC related classes
+
+
+
