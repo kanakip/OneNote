ead91b9ec66bd7995df3a2b11c0eec516874d988
kanakip <pallavikanaki13@gmail.com>
2025-05-09 13:22:52 +0530

    Create Error Code.java

---
+++ Java/Error Code.java
@@ -0,0 +1,10 @@
+//FIG
+
+HTTP status codes 400 and 500 indicate that something is wrong with a web request or the server: 
+
+• 400 Bad Request
+The client's request has incorrect syntax. The client needs to correct the request and try again. 
+• 500 Internal Server Error
+The server encountered an unexpected condition and was unable to complete the request. Developers or administrators need to resolve the issue on the server. 
+
+From <https://www.google.com/search?sca_esv=31c7c02de220de3d&sxsrf=ADLYWILRXaz5V1_Ly84iLdjaj-p4nNvcbA:1731741989163&q=error+code+400+and+500%27&source=lnms&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J603JUkR9Y5suk8yuy50qOYMMWTNCTu57lKPsZpPcfqPO_IpBTFq5Iu5fjJcS9zeIjPFJUv87eZfN29UFdVUPMDQu7RK9R_bUPLOf1fjQfA8iavGDDNI4XAiPb7CBH_tYixgN7oUspZLXXXsRkqTIf5aqMzAsa5BiGd_cq9iXccaPMpaPw&sa=X&ved=2ahUKEwjj_ezQqeCJAxXjulYBHcY8LNYQ0pQJegQIDhAB&biw=1536&bih=695&dpr=1.25> 
