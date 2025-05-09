dd806c9dcd36d8a55a85ea320a8187898d74be80
kanakip <pallavikanaki13@gmail.com>
2025-05-09 12:59:39 +0530

    Create Inheritance.java

---
+++ Java/Inheritance.java
@@ -0,0 +1,21 @@
+# Inheritance Types in Java
+
+In Java, inheritance is a key feature of object-oriented programming that allows a new class to inherit properties and methods from an existing class. There are several types of inheritance:
+
+1.  Single Inheritance : A class inherits from one superclass. This is the simplest form of inheritance.
+   - Example: Class `A` is a superclass, and Class `B` inherits from it.
+
+2.  Multilevel Inheritance : A class acts as a superclass for another class, creating a chain of inheritance.
+   - Example: Class `A` → Class `B` → Class `C`, where `C` inherits from `B`, and `B` inherits from `A`.
+
+3.  Hierarchical Inheritance : Multiple subclasses inherit from the same superclass.
+   - Example: Class `A` is the parent class, and both Class `B` and Class `C` inherit from `A`.
+
+4.  Multiple Inheritance : A class can inherit from more than one superclass. Java does not support this directly to avoid ambiguity but allows it through interfaces.
+   - Example: Class `B` can implement multiple interfaces that define methods from different classes 
+
+5.  Hybrid Inheritance : A combination of two or more types of inheritance. It can be achieved through interfaces 
+				
+
+
+// FIG
