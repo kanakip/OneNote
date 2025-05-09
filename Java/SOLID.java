a9547dfcfd98de6a6c3cd3d573d978d9d8e6e6d7
kanakip <pallavikanaki13@gmail.com>
2025-05-09 13:13:29 +0530

    Create SOLID.java

---
+++ Java/SOLID.java
@@ -0,0 +1,8 @@
+Single Responsibility Principle
+The single responsibility principle states that every Java class must perform a single functionality. Implementation of multiple functionalities in a single class mashup the code and if any modification is required may affect the whole class. It precise the code and the code can be easily maintained. 
+
+From <https://www.javatpoint.com/solid-principles-java> 
+
+https://www.javatpoint.com/solid-principles-java
+
+https://www.educative.io/answers/what-are-the-solid-principles-in-java
